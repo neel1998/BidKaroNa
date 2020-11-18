ee4e98feb4615d90edddb257aaec8bab339621b4
neel1998 <neellovepainting@gmail.com>
2020-11-18 12:17:21 +0530

    removed wallet details from home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -21,18 +21,11 @@ export default class Home extends Component {
               history = {this.props.history}/>
             <br/>
             <div>
-              <div style = {{'width' : '60%', 'background' : '#f5f5f5', 'float':'left', 'margin' : '10px', 'borderRadius' : '20px', 'padding' : '20px', 'textAlign' :'left', 'color' : '#006064'}}>
+              <div style = {{'width' : '95%', 'background' : '#f5f5f5', 'margin' : '10px', 'borderRadius' : '20px', 'padding' : '20px', 'textAlign' :'left', 'color' : '#006064'}}>
                 <h3>What is BidKaroNa?</h3>
                 <p>BidKaroNa is a Blockchain based auction system</p>
                 <p>This system will allow sellers to conduct online auction in a complete decentralized manner without relying on any central auction service</p>
                 <p>With the very minimalistic and simple user interface of BidKaroNa users can start their decentralized auctions almost immediately</p>
-              </div>
-              <div style = {{'width' : '30%', 'background' : '#f5f5f5', 'display' : 'inline-block', 'margin' : '10px', 'borderRadius' : '20px', 'padding' : '20px' , 'textAlign' :'left', 'color' : '#006064'}}>
-                <h3>Linked Wallet Status</h3>
-                <hr style = {{'width' : '100%'}}/>
-                <p>This will contain details about the linked wallet</p>
-                <p><b>Wallet Address:</b></p>
-                <p><b>Wallet Balance:</b></p>
               </div>
             </div>
             <div style = {{'position' : 'absolute', 'bottom': '10px', 'width' : '100%'}}>
